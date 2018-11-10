681d777225ce526851f15a042d93feeab38a1ae5
Heinner Zapata <heinnerzapata1991@gmail.com>
2018-11-10 16:21:29 -0500

    fixing port

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/api/test1', (req, res) => {
     newElement
   });
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
