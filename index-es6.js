b0b8109463902aece781db4e8650dd5eeac7b1d4
Heinner Zapata <heinnerzapata1991@gmail.com>
2018-11-17 16:37:49 -0500

    basic response

--- index-es6.js
+++ index-es6.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (reqq, res) => {
+app.get('/', (req, res) => {
   res.status(200).send({
     success: 'true',
     message: 'welcome to api',
@@ -15,42 +15,6 @@ app.get('/', (reqq, res) => {
   });
 });
 
-app.get('/api/test1', (reqq, res) => {
-  res.status(200).send({
-    success: 'true',
-    message: 'test1 retrieved successfully',
-    res1: db
-  });
-});
-
-app.post('/api/test1', (req, res) => {
-  if(!req.body.title) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'title is required'
-    });
-  }
-  else if(!req.body.description) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'description is required'
-    });
-  }
-
-  const newElement = {
-    id: db.length + 1,
-    title: req.body.title,
-    description: req.body.description
-  };
-
-  db.push(newElement);
-
-  return res.status(201).send({
-    success: 'true',
-    message: 'new element added succesfully',
-    newElement
-  });
-});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var app = (0, _express2.default)();
 app.use(_bodyParser2.default.json());
 app.use(_bodyParser2.default.urlencoded({ extended: false }));
 
-app.get('/', function (reqq, res) {
+app.get('/', function (req, res) {
   res.status(200).send({
     success: 'true',
     message: 'welcome to api',
@@ -27,41 +27,6 @@ app.get('/', function (reqq, res) {
   });
 });
 
-app.get('/api/test1', function (reqq, res) {
-  res.status(200).send({
-    success: 'true',
-    message: 'test1 retrieved successfully',
-    res1: _db2.default
-  });
-});
-
-app.post('/api/test1', function (req, res) {
-  if (!req.body.title) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'title is required'
-    });
-  } else if (!req.body.description) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'description is required'
-    });
-  }
-
-  var newElement = {
-    id: _db2.default.length + 1,
-    title: req.body.title,
-    description: req.body.description
-  };
-
-  _db2.default.push(newElement);
-
-  return res.status(201).send({
-    success: 'true',
-    message: 'new element added succesfully',
-    newElement: newElement
-  });
-});
 var PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function () {
