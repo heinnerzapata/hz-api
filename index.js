e1e8ea08792771ce29520662437fe167d5816788
Heinner Zapata <heinnerzapata1991@gmail.com>
2018-11-25 12:47:58 -0500

    adding cors

--- index.js
+++ index.js
@@ -7,19 +7,21 @@
 var express = require('express');
 var db = require('./db/db');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 
 var app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.use(express.static(__dirname + '/static'));
 
